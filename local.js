c4e5f33958e17f8c60befd8c5594e12923b344f5
anilkharatmol <122686374+anilkharatmol@users.noreply.github.com>
2023-01-19 13:27:24 +0530

    scale app to more users

--- local.js
+++ local.js
@@ -1,10 +1,19 @@
-function getacall(){
+let form=document.getElementById('myform');
+let itemList=document.getElementById('userlist');
+
+
+form.addEventListener('submit',myform);
+
+
+function myform(e){
+    e.preventDefault();
     let name=document.getElementById('name');
     let mail=document.getElementById('email');
     let contact=document.getElementById('number');
     let date=document.getElementById('date');
     let time=document.getElementById('time');
 
+    
     let obj={
         name:name.value,
         email:mail.value,
@@ -12,5 +21,23 @@ function getacall(){
         date:date.value,
         time:time.value
     };
-    localStorage.setItem('obj',JSON.stringify(obj));
+
+    localStorage.setItem(document.getElementById('email').value,JSON.stringify(obj));
+
+   
+    let ul=document.getElementById('userlist');
+    let li=document.createElement('li');
+    li.className='user-group';
+    li.appendChild(document.createTextNode(document.getElementById('name').value));
+    li.appendChild(document.createTextNode('  '));
+    li.appendChild(document.createTextNode(document.getElementById('email').value));
+    li.appendChild(document.createTextNode('  '));
+    li.appendChild(document.createTextNode(document.getElementById('number').value));
+    li.appendChild(document.createTextNode('  '));
+    li.appendChild(document.createTextNode(document.getElementById('date').value));
+    li.appendChild(document.createTextNode('  '));
+    li.appendChild(document.createTextNode(document.getElementById('time').value));
+    ul.appendChild(li);
 };
+
+    
